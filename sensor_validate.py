67ea74f45ceec0a1064488dbe312e965efcfe207
Vivek Tak <57911834+wanderer-soul92@users.noreply.github.com>
2021-03-24 19:31:11 +0530

    Add files via upload

--- sensor_validate.py
+++ sensor_validate.py
@@ -1,19 +1,20 @@
 
-def  _give_me_a_good_name(value, nextValue, maxDelta):
+def  in_tolerance_level(value, nextValue, maxDelta):
   if nextValue - value > maxDelta:
     return False
   return True
 
-def validate_soc_reading(values):
+def validate_reading(values,parameter):
+  threshold = { 'soc': 0.05, 'current': 0.1}
   last_but_one_reading = len(values) - 1
   for i in range(last_but_one_reading):
-    if(not _give_me_a_good_name(values[i], values[i + 1], 0.05)):
+    if(not in_tolerance_level(values[i], values[i + 1], threshold[parameter])):
       return False
   return True
 
-def validate_current_reading(values):
+"""def validate_current_reading(values):
   last_but_one_reading = len(values) - 1
   for i in range(last_but_one_reading):
     if(not _give_me_a_good_name(values[i], values[i + 1], 0.1)):
       return False
-  return True
+  return True """
