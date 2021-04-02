f4a1d39d15410e801f8bb5ea6577f47da2368cb8
Vivek Tak <57911834+wanderer-soul92@users.noreply.github.com>
2021-04-02 16:54:51 +0530

    Update sensor_validate.py
    deleted the commnets

--- sensor_validate.py
+++ sensor_validate.py
@@ -18,10 +18,3 @@ def validate_Para_reading(values,parameter):
     if(not in_permissible_range(values[i], values[i + 1], threshold[parameter])):
       return False
   return True
-
-"""def validate_current_reading(values):
-  last_but_one_reading = len(values) - 1
-  for i in range(last_but_one_reading):
-    if(not _give_me_a_good_name(values[i], values[i + 1], 0.1)):
-      return False
-  return True """
